68047e36a7ef0df89a08b5a2ce1ff988138c16fb
KirNsk123 <kirill.marchenko2k10@gmail.com>
2024-06-03 19:08:14 +0700

    0.2
    Server's scrip's structure refactored

--- Server.py
+++ Server.py
@@ -1,6 +1,27 @@
 import socket
 import time
 
+def recive_data():
+    data_rcv = conn.recv(1024).decode('utf-8')
+    if data_rcv == "Client closed connection":
+        print("Клиент разорвал соединие...")
+        conn.close()
+        return 1
+    elif not data_rcv:
+        return 1
+    else:
+        return f"Клиент: {data_rcv}"
+
+def send_data():
+    data_snd = input("Вы: ").encode('utf-8')
+    if data_snd.decode('utf-8') == "конец":
+        conn.send("Server closed connection".encode('utf-8'))
+        conn.close()
+        return 1
+    else:
+        return data_snd
+
+
 print("Сервер запущен")
 time.sleep(0.5)
 print("Поиск соединения...")
@@ -10,24 +31,28 @@ srv.bind(('', 9090))
 srv.listen(1)
 conn, addr = srv.accept()
 
-print('connected:', addr)
+print('Соединение найдено!', addr)
 
 while True:
-    data_rcv = conn.recv(1024).decode('utf-8')
-    if data_rcv == "Client closed connection":
-        print("Клиент разорвал соединие...")
-        conn.close()
-        break
-    else:
-        print("Клиент:", data_rcv)
-    if not data_rcv:
-        break
-    data_snd = input("Вы: ").encode('utf-8')
-    if data_snd.decode('utf-8') == "конец":
-        conn.send("Server closed connection".encode('utf-8'))
-        conn.close()
-        break
-    else:
-        conn.send(data_snd)
+#    data_rcv = conn.recv(1024).decode('utf-8')
+#    if data_rcv == "Client closed connection":
+#        print("Клиент разорвал соединие...")
+#        conn.close()
+#        break
+#    else:
+#        print("Клиент:", data_rcv)
+#    if not data_rcv:
+#        break
+    data_rcv = recive_data()
+    print(data_rcv)
+#    data_snd = input("Вы: ").encode('utf-8')
+#    if data_snd.decode('utf-8') == "конец":
+#        conn.send("Server closed connection".encode('utf-8'))
+#        conn.close()
+#        break
+#    else:
+#        conn.send(data_snd)
+    data_snd = send_data()
+    conn.send(data_snd)
 
 conn.close()
